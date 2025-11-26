097927fe3d4dab2176885f55a8ea5217f1d65047
Chifuyu <admin@kyllianrmkn.fr>
2025-11-26 14:52:57 +0100

    Feat : Name post API in Progress

--- http.go
+++ http.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/Hello", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 
 	})
 
 	log.Print("Listening on port 8080")
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type User struct {
+	Name string `json:"name"`
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 }
 
 // Output: Hello, World! (at http://localhost:8080/Hello)
