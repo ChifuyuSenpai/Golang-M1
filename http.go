bf7e3c45d80eccbfa8f2e5bc9f12c49a96c1ceba
Chifuyu <admin@kyllianrmkn.fr>
2025-11-26 14:25:18 +0100

    fixup! Feat : first main project

---
+++ http.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/Hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+
+	})
+
+	log.Print("Listening on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
