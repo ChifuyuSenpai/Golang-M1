39f529e4ce59ac14dde2d1e399e8dec988565350
Chifuyu <admin@kyllianrmkn.fr>
2025-11-27 15:02:48 +0100

    feat: Changing current system to json response / archive current system

--- TP-HTTP-API/main.go
+++ TP-HTTP-API/main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
-	//	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
-	//	"os"
-	//	"time"
 )
 
 func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(time.Now().Format(time.DateTime)))
-		//		w.WriteHeader(os.)
+		hostname, err := os.Hostname()
+		if err != nil {
+			hostname = "unknown"
+		}
+		//////////////////
+		// Premier try en front sans json
+		//pid := os.Getpid()
+		////response := fmt.Fprintf("Hostname: " + hostname + " | PID: " + string(rune(pid)) + " | Status: OK"), 2éme try
+		//response := fmt.Sprintf("Time: %s | Hostname: %s | PID: %d | Status: OK", // \ IA
+		//	time.Now().Format(time.DateTime),
+		//	hostname,
+		//	pid)
+		//
+		//w.Write([]byte(response))
+
+		//w.Write([]byte(time.Now().Format(time.DateTime))) // 1er try
+		//w.Write([]byte(" Status : OK"))
+		//
+		///////////////////
+
 	})
 
 	log.Print("Service UP » Listening on port 8443 !")
